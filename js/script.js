0655c8c28c6f8ae68b7eb8263e042c9c81a91904
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-02-06 15:54:15 +0000

    Change the input type to number

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const bill = parseInt(document.querySelector('#bill'));
+const bill = document.querySelector('#bill');
 const tipPercentage = document.getElementsByClassName('tips');
 const numberOfPersons = document.querySelector('#number');
 const tipAmount = document.querySelector('.amount h1');
@@ -9,8 +9,8 @@ Array.from(tipPercentage).forEach(e => {
     e.addEventListener("click", (e) => {
         let target = e.target.textContent;
         target = parseInt(target);
-        let tip = target * bill.value
-        console.log(tip);
+        let tip = target * bill;
+        console.log(typeof tip);
         console.log(typeof target);
     })
 
