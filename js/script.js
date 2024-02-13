83f28867a6a64e046a5f074d5453fcf94d8dc9d4
KwameWilson <kwamidzorgbenyuie@gmail.com>
2024-02-13 12:39:22 +0000

    Recommit

--- js/script.js
+++ js/script.js
@@ -6,69 +6,64 @@ const tipAmountPerPerson = document.querySelector('.amount h1');
 const totalPerPersons = document.querySelector('.total h1');
 const reset = document.querySelector('button.btn');
 let amount;
-let messages =
 
-    Array.from(tipPercentage).forEach(e => {
-        e.addEventListener("click", (e) => {
+Array.from(tipPercentage).forEach(e => {
+    e.addEventListener("click", (e) => {
 
-            const setError = (element, message) => {
-                const inputTab = element.parentElement;
-                const errorDisplay = inputTab.querySelector('.error');
+        const setError = (element, message) => {
+            const inputTab = element.parentElement;
+            const errorDisplay = inputTab.querySelector('.error');
 
-                errorDisplay.innerText = message;
-                inputTab.classList.add('error');
-                inputTab.classList.remove('success');
+            errorDisplay.innerText = message;
+            inputTab.classList.add('error');
+            inputTab.classList.remove('success');
+        }
+
+        const setSuccess = element => {
+            const inputTab = element.parentElement;
+            const errorDisplay = inputTab.querySelector('.error');
+
+            errorDisplay.textContent = "";
+            errorDisplay.classList.add('success');
+            errorDisplay.classList.remove('error');
+        }
+
+        const validateInputs = () => {
+            const billValue = bill.value;
+            const numberOfPersonsValue = numberOfPersons.value;
+
+            if (billValue === '') {
+                console.log('Input fields cannot be left blank');
+                setError(bill, "Input fields cannot be left blank");
+            } else {
+                setSuccess(bill);
             }
 
-            // How did the .error classList end up on the errorDisplay Div
-
-
-
-            const setSuccess = element => {
-                const inputTab = element.parentElement;
-                const errorDisplay = inputTab.querySelector('.error');
-
-                errorDisplay.textContent = "";
-                errorDisplay.classList.add('success');
-                errorDisplay.classList.remove('error');
+            if (numberOfPersonsValue === '') {
+                console.log('Input fields cannot be left blank');
+                setError(numberOfPersons, "Input fields cannot be left blank");
+            } else {
+                setSuccess(numberOfPersons);
             }
 
-            const validateInputs = () => {
-                const billValue = bill.value;
-                const numberOfPersonsValue = numberOfPersons.value;
+        }
 
-                if (billValue === '') {
-                    console.log('Input fields cannot be left blank');
-                    setError(bill, "Input fields cannot be left blank");
-                } else {
-                    setSuccess(bill);
-                }
-
-                if (numberOfPersonsValue === '') {
-                    console.log('Input fields cannot be left blank');
-                    setError(numberOfPersons, "Input fields cannot be left blank");
-                } else {
-                    setSuccess(numberOfPersons);
-                }
-
-            }
-
-            validateInputs()
+        validateInputs()
 
 
-            let target = e.target.textContent;
-            target = parseInt(target) / 100;
+        let target = e.target.textContent;
+        target = parseInt(target) / 100;
 
-            let tip = target * bill.value;
-            amount = tip + parseInt(bill.value);
+        let tip = target * bill.value;
+        amount = tip + parseInt(bill.value);
 
-            let tipPerPerson = tip / numberOfPersons.value;
+        let tipPerPerson = tip / numberOfPersons.value;
 
-            tipAmountPerPerson.textContent = tipPerPerson.toFixed(2);
-            totalPerPersons.textContent = amount.toFixed(2);
+        tipAmountPerPerson.textContent = tipPerPerson.toFixed(2);
+        totalPerPersons.textContent = amount.toFixed(2);
 
-        })
-    });
+    })
+});
 
 customTipPercentage.addEventListener('change', () => {
     let customValue = customTipPercentage.value;
